b293b66d41c21e6da763eccce88281dd05c384e5
Ivan <ivan.shalnov@gmail.com>
2024-07-19 18:41:04 +0300

    stable version

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './src')
 from downloadAudio import getTextWithGPT
 from downloadSubs import downloadSubs
 
-print('hello from app.py')
-
 vids = [
     
 ]
